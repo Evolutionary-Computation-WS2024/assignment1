90fe923f6c019b257f85e6fe6d67cab147b0b9a5
JerzyJerzu <PJ@pawlik.info.pl>
2024-10-09 17:55:06 +0200

    implemented NNtoAny, but not completed

--- src/main/java/evolcomp/strategy/NNToAnyNodeStrategy.java
+++ src/main/java/evolcomp/strategy/NNToAnyNodeStrategy.java
@@ -2,13 +2,24 @@ package evolcomp.strategy;
 
 import evolcomp.tsp.Cycle;
 import evolcomp.tsp.TSPInstance;
+import evolcomp.strategy.NNToAnyUtils.Patch;    
+
 
 public final class NNToAnyNodeStrategy extends Strategy {
     public NNToAnyNodeStrategy() {}
-
+    
     // TODO: Implement
     @Override
     public Cycle apply(final TSPInstance tspInstance, final int startNode) {
-        return null;
+        
+        int no_nodes = tspInstance.getHowManyNodes();
+        int half_no_nodes = (int) Math.ceil(no_nodes / 2.0);
+        
+        Patch patch = new Patch(startNode, tspInstance);
+        
+        for (int i = 2; i <= half_no_nodes; i++) {
+            patch.extend();
+        }
+        return new Cycle(patch.toList());
     }
 }

a6503b57dae3ce9a70ae47508d045f8d6b66539a
JerzyJerzu <PJ@pawlik.info.pl>
2024-10-19 23:45:03 +0200

    Revert "changed NNtoAny to greedy cycle"
    This reverts commit 452e1fc9a06f1c1439e665258ebc49be1827347c.

--- src/main/java/evolcomp/strategy/NNToAnyUtils/Extension.java
+++ src/main/java/evolcomp/strategy/NNToAnyUtils/Extension.java
@@ -5,7 +5,6 @@
 package evolcomp.strategy.NNToAnyUtils;
 
 import evolcomp.tsp.TSPInstance;
-import evolcomp.strategy.NNToAnyUtils.Patch;
 
 /**
  *
@@ -16,12 +15,10 @@ public class Extension {
     public final int extra_node_id;
     public final evolcomp.strategy.NNToAnyUtils.Node anchor;
     private final TSPInstance instance;
-    private final Patch path;
-    public Extension(TSPInstance instance, evolcomp.strategy.NNToAnyUtils.Node anchor, int extra_node_id, Patch path) {
+    public Extension(TSPInstance instance, evolcomp.strategy.NNToAnyUtils.Node anchor, int extra_node_id) {
         this.extra_node_id = extra_node_id;
         this.anchor = anchor;
         this.instance = instance;
-        this.path = path;
         this.utility_difference = this.computeUtilityDifference();
     }
     private int computeUtilityDifference() {
@@ -32,14 +29,7 @@ public class Extension {
             int gains = this.instance.getDistanceBetween(anchor.point_id, anchor.getNext().point_id);
             return costs-gains;
         }
-        else if (anchor == path.starting_node){
-            return costs; 
-        }
-        else{
-            costs += this.instance.getDistanceBetween(extra_node_id, path.starting_node.point_id);
-            int gains = this.instance.getDistanceBetween(anchor.point_id, path.starting_node.point_id);
-            return costs-gains;
-        }
+        return costs;
     }
     public void add_to_the_patch() {
         if (this.anchor.hasNext()) {
